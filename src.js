5185d3872b6d9fd83952a0db6708ccf00283a52f
Laura Vas <v.luva99@gmail.com>
2021-03-09 19:49:11 +0100

    all cells are generated

--- src.js
+++ src.js
@@ -1,22 +1,37 @@
 
-let cell_clone;
-let cell_row_clone;
+let clone;
+let i, j;
 
-for (i = 1; i < 11; i++) {
-    cell_row_clone = document.querySelector('#cell-row-0').cloneNode(true);
-    // Change the id attribute of the newly created cell
-    cell_row_clone.setAttribute('id', 'cell-row-' + i);
-    // Add the clone to the html
-    document.querySelector('.table').appendChild(cell_row_clone);
+for (i = 0; i < 11; i++) {
+
+    /*if (i === 0){
+        for (j = 1; j < 10; j++) {
+            cloneElement('#cell-0_0', 'cell_' + i + '-' + j, '#cell-row-' + i);
+        }
+        i++;
+        continue;
+    }*/
+
+    cloneElement('#cell-row-0', 'cell-row-' + i, '.table');
 
     for (j = 1; j < 10; j++) {
-        // Create a clone of the cell
-        cell_clone = document.querySelector('#cell-0_0').cloneNode(true);
-        // Change the id attribute of the newly created cell
-        cell_clone.setAttribute('id', 'cell_' + i + '_' + j);
-        // Add the clone to the html
-        document.querySelector('#cell-row-' + i).appendChild(cell_clone);
+        cloneElement('#cell-0_0', 'cell_' + i + '-' + j, '#cell-row-' + i);
     }
+}
+
+function showContent() {
+    var temp = document.getElementsByTagName("template")[0];
+    var clon = temp.content.cloneNode(true);
+    document.body.appendChild(clon);
+}
+
+function cloneElement(selector, idValue, parentSelector){
+    // Create a clone of the cell
+    clone = document.querySelector(selector).cloneNode(true);
+    // Change the id attribute of the newly created cell
+    clone.setAttribute('id', idValue);
+    // Add the clone to the html
+    document.querySelector(parentSelector).appendChild(clone);
 }
 
 function clickCell() {
