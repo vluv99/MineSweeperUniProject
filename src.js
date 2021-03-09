860d3d86d4bc01ac66db936a806466031b9e0bef
Laura Vas <v.luva99@gmail.com>
2021-03-09 21:38:02 +0100

    Fixed generation
    changed into using templates
    click to animate works
    edited cloneElement function to fit to the template
    fixed individual ID problem for cells

--- src.js
+++ src.js
@@ -4,18 +4,16 @@ let i, j;
 
 for (i = 0; i < 10; i++) {
 
-    if (i < 1){
-        for (j = 1; j < 10; j++) {
-            cloneElement('#cell-0_0', 'cell_' + i + '-' + j, '#cell-row-' + i);
-        }
-        continue;
-    }
-
-    cloneElement('#cell-row-0', 'cell-row-' + i, '.table');
-
-    /*for (j = 1; j < 10; j++) {
-        cloneElement('#cell-0_0', 'cell_' + i + '-' + j, '#cell-row-' + i);
+   /* for (j = 1; j < 10; j++) {
+        cloneElement('#template-cell', 'cell_' + i + '-' + j, '#cell-row-' + i);
+        //clickCell('cell_' + i + '-' + j);
     }*/
+
+    cloneElement('#template-row', '#cell-row-0','cell-row-' + i, '.table');
+
+    for (j = 0; j < 10; j++) {
+        cloneElement('#template-cell', '#cell-0_0', 'cell_' + i + '-' + j, '#cell-row-' + i);
+    }
 }
 
 function showContent() {
@@ -24,16 +22,17 @@ function showContent() {
     document.body.appendChild(clon);
 }
 
-function cloneElement(selector, idValue, parentSelector){
+function cloneElement(selector, innerSelector, idValue, parentSelector) {
     // Create a clone of the cell
-    clone = document.querySelector(selector).cloneNode(true);
+    clone = document.querySelector(selector).content.cloneNode(true);
     // Change the id attribute of the newly created cell
-    clone.setAttribute('id', idValue);
+    clone.querySelector(innerSelector).setAttribute('id', idValue);
     // Add the clone to the html
     document.querySelector(parentSelector).appendChild(clone);
 }
 
-function clickCell() {
-    var element = document.getElementById("cell-0_0-cover");
-    element.classList.add("cell-cover__hidden");
+// Gives the cell the class to animate when clicked
+function clickCell(cell) {
+    //var element = document.getElementById(elementId);
+    cell.querySelector("#cell-0_0-cover").classList.add("cell-cover__hidden");
 }
